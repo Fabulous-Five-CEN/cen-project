20995a1a4127076664be9ecafe6edf6fc4fa5236
Elik Lipskar <eliklipskar@gmail.com>
2025-11-11 14:37:31 -0500

    Update main's routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,6 +2,5 @@ from flask import render_template
 from . import main_bp
 
 @main_bp.route("/")
-@main_bp.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html", page_title="Dashboard")
+    return render_template("dashboard.html")
